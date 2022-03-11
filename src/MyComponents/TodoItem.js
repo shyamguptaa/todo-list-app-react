05da8500f670e438849b0db10f6773a525c39c4d
Shyam <89705591+shyamguptaa@users.noreply.github.com>
2022-03-11 17:22:12 +0530

    fetch data inside table

--- src/MyComponents/TodoItem.js
+++ src/MyComponents/TodoItem.js
@@ -2,30 +2,53 @@ import React from "react";
 
 export const TodoItem = (props) => {
   return (
-    console.log(props.todo.title),
-    (
-      <>
-        <div className="container text-center">
-          <table className="table">
-            <thead>
-              <th scope="col"> #</th>
-              <th scope="col"> Title</th>
-              <th scope="col"> Desc</th>
-              <th scope="col"> Action</th>
-            </thead>
-            <tbody>
+    // console.log(props.todo),
+
+    // console.log(dtaa)
+    <>
+      <div className="container text-center">
+        <table className="table">
+          <thead>
+            <th scope="col"> #</th>
+            <th scope="col"> Title</th>
+            <th scope="col"> Desc</th>
+            <th scope="col"> Action</th>
+          </thead>
+          <tbody>
+            {props.todo.map((dtaa) => (
               <tr>
-                <th scope="row">{props.todo.sno}</th>
-                <td>{props.todo.title}</td>
-                <td>{props.todo.desc}</td>
+                <th scope="row">{dtaa.sno}</th>
+                <td>{dtaa.title}</td>
+                <td>{dtaa.desc}</td>
                 <td>
                   <button className="btn btn-sm btn-danger">Delete</button>
                 </td>
               </tr>
-            </tbody>
-          </table>
-        </div>
-      </>
-    )
+            ))}
+          </tbody>
+        </table>
+      </div>
+      {/* single item todo list  */}
+      {/* <div className="container text-center">
+        <table className="table">
+          <thead>
+            <th scope="col"> #</th>
+            <th scope="col"> Title</th>
+            <th scope="col"> Desc</th>
+            <th scope="col"> Action</th>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">{props.todo.sno}</th>
+              <td>{props.todo.title}</td>
+              <td>{props.todo.desc}</td>
+              <td>
+                <button className="btn btn-sm btn-danger">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div> */}
+    </>
   );
 };
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -5,9 +5,9 @@ export const Todos = (props) => {
   return (
     <div className="conatiner">
       <h3 className="text-center mt-3">Todo List</h3>
-      {console.log(props.todos)}
+      {/* {console.log(props.todos)} */}
 
-      <TodoItem todo={props.todos[0]} />
+      <TodoItem todo={props.todos} />
     </div>
   );
 };
